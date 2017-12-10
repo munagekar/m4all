dc2ee9788ad02505b500b6e46d9e684ec1abbe01
munagekar <avm.abhishek@gmail.com>
2017-12-11 02:26:38 +0530

    First Window: Testing Stuff

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
 
+class MainDisplay(Widget):
+    print ('I reached here')
+    pass
 
-class LoginScreen(GridLayout):
-
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
-
-
-class MyApp(App):
-
+class M4AllApp(App):
     def build(self):
-        return LoginScreen()
+        return MainDisplay()
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    M4AllApp().run()
