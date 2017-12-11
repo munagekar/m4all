2440f1abe23d40081d74085ad657be2b3777fa39
munagekar <avm.abhishek@gmail.com>
2017-12-12 02:18:06 +0530

    Adding Last FM Json Parsing

--- main.py
+++ main.py
@@ -3,11 +3,27 @@ from kivy.uix.widget import Widget
 from kivy.network.urlrequest import UrlRequest
 from kivy.uix.recycleview import RecycleView
 
+import json
+
+class Track:
+    name = None
+    artist = None
+    coverartlink = None
+    url= None
+
+    def __init__(self,name=None,artist=None,coverartlink=None,url=None):
+        self.name=name
+        self.artist=artist
+        self.coverartlink=coverartlink
+        self.url=url
 
 class RV(RecycleView):
     def __init__(self, **kwargs):
         super(RV, self).__init__(**kwargs)
-        self.data = [{'text': str(x)} for x in range(100)]
+        #This is required
+        #[{'text': '0'}, {'text': '1'}, {'text': '2'}]
+        self.data = [{'text': str(x)} for x in range(3)]
+        
 
 class MainDisplay(Widget):
     pass
@@ -17,13 +33,36 @@ class M4AllApp(App):
         mainDisplay = MainDisplay()
         return mainDisplay
 
-def got_json(req, result):
-    print (result)
+def topTrackParser(req, result):
+    
+    jsonresult = result
+    jsontracklist = jsonresult['tracks']['track']
+    tracks =[]
+    for i in range(1):
+        jsontrack =jsontracklist[i]
+        trackname = jsontrack['name']
+        print(trackname)
+        trackartist = jsontrack['artist']['name']
+        print(trackartist)
+        trackurl = jsontrack['url']
+        print(trackurl)
+        trackart = jsontrack['image'][1]['#text']
+        print(trackart)
+        tracks.append(Track(trackname,trackartist,trackart,trackurl))
+
+    
+
+def getTopTracks():
+    result = UrlRequest('http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=f420e235b5a52c620114d88c28b6f186&format=json', on_success=topTrackParser) 
+    
+
+
 
 
 
 
 if __name__ == '__main__':
-    req = UrlRequest('http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=f420e235b5a52c620114d88c28b6f186&format=json', on_success=got_json) 
+    
+    getTopTracks()
     M4AllApp().run()
     
