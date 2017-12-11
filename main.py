d9dc3e73eb05ddc84811c4bbe4bf757ac1a185ec
munagekar <avm.abhishek@gmail.com>
2017-12-12 00:30:18 +0530

    Added Recycler View

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.network.urlrequest import UrlRequest
+from kivy.uix.recycleview import RecycleView
+
+
+class RV(RecycleView):
+    def __init__(self, **kwargs):
+        super(RV, self).__init__(**kwargs)
+        self.data = [{'text': str(x)} for x in range(100)]
 
 class MainDisplay(Widget):
-    print ('I reached here')
     pass
 
 class M4AllApp(App):
@@ -12,11 +18,12 @@ class M4AllApp(App):
         return mainDisplay
 
 def got_json(req, result):
-    print ('Reached here')
     print (result)
 
 
+
+
 if __name__ == '__main__':
-    req = UrlRequest('http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=india&api_key=f420e235b5a52c620114d88c28b6f186&format=json', on_success=got_json)
+    req = UrlRequest('http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=f420e235b5a52c620114d88c28b6f186&format=json', on_success=got_json) 
     M4AllApp().run()
     
