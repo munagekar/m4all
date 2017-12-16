853bac864cc984215e0aabc32ccab8c5f6307710
munagekar <avm.abhishek@gmail.com>
2017-12-16 23:00:42 +0530

    Added feature to see the top songs of the week

--- lfm.py
+++ lfm.py
@@ -1,35 +1,42 @@
 from kivy.network.urlrequest import UrlRequest
 
+API_key = ""
+topTrackCallback = None
 
 class Track:
-    name = None
-    artist = None
-    coverartlink = None
-    url= None
+	name = None
+	artist = None
+	coverartlink = None
+	url= None
 
-    def __init__(self,name=None,artist=None,coverartlink=None,url=None):
-        self.name=name
-        self.artist=artist
-        self.coverartlink=coverartlink
-        self.url=url
+	def __init__(self,name=None,artist=None,coverartlink=None,url=None):
+		self.name=name
+		self.artist=artist
+		self.coverartlink=coverartlink
+		self.url=url
 
-def getTopTracks():
-	result = UrlRequest('http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=f420e235b5a52c620114d88c28b6f186&format=json', on_success=topTrackParser) 
+def getTopTracks(callback):
+	global topTrackCallback
+	#print (callback)
+	topTrackCallback=callback	
+	result = UrlRequest('http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key='+API_key+'&format=json', on_success=topTrackParser)
 
-def topTrackParser(req, result):
-    jsonresult = result
-    jsontracklist = jsonresult['tracks']['track']
-    tracks =[]
-    for i in range(1):
-        jsontrack =jsontracklist[i]
-        trackname = jsontrack['name']
-        #print(trackname)
-        trackartist = jsontrack['artist']['name']
-        #print(trackartist)
-        trackurl = jsontrack['url']
-        #print(trackurl)
-        trackart = jsontrack['image'][1]['#text']
-        #print(trackart)
-        tracks.append(Track(trackname,trackartist,trackart,trackurl))
+def topTrackParser(req,result):
+	global topTrackCallback
+	jsonresult = result
+	jsontracklist = jsonresult['tracks']['track']
+	tracks =[]
+	for i in range(50):
+		jsontrack =jsontracklist[i]
+		trackname = jsontrack['name']
+		#print(trackname)
+		trackartist = jsontrack['artist']['name']
+		#print(trackartist)
+		trackurl = jsontrack['url']
+		#print(trackurl)
+		trackart = jsontrack['image'][3]['#text']
+		#print(trackart)
+		tracks.append(Track(trackname,trackartist,trackart,trackurl))
+	topTrackCallback(tracks)
 
 
--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.recycleview import RecycleView
-
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
 import lfm
+import os
 
+cache_folder= None
 
 class RV(RecycleView):
     def __init__(self, **kwargs):
         super(RV, self).__init__(**kwargs)
         #This is required
         #[{'text': '0'}, {'text': '1'}, {'text': '2'}]
-        self.data = [{'text': str(x)} for x in range(3)]
+        lfm.getTopTracks(self.datafiller)
+        self.data = []
+    def datafiller(self,tracks):
+        global cache_folder
+        print ('Value of Cache_folder is')
+        print (cache_folder)
+        self.data =[{'displaytext':"[b]"+tracks[i].name+"\n-"+tracks[i].artist+"[/b]",'imagelink':(tracks[i].coverartlink).replace("https:","http:")} for i in range(len(tracks))]
+
+
+class GridItem(FloatLayout):
+    displaytext =StringProperty()
+    imagelink = StringProperty()
+    #aimg = AsyncImage(source=imagelink)
+    pass
         
 
 class MainDisplay(Widget):
@@ -19,8 +37,17 @@ class MainDisplay(Widget):
 class M4AllApp(App):
     def build(self):
         mainDisplay = MainDisplay()
+        self.initilize_global_vars()
         return mainDisplay
 
+    def initilize_global_vars(self):
+        global root_folder
+        global cache_folder
+        root_folder = self.user_data_dir
+        cache_folder = os.path.join(root_folder,'cache')
+
+        if not os.path.exists(cache_folder):
+            os.makedirs(cache_folder)
 
 
     
@@ -35,6 +62,6 @@ class M4AllApp(App):
 
 if __name__ == '__main__':
     
-    lfm.getTopTracks()
+    #lfm.getTopTracks()
     M4AllApp().run()
     
